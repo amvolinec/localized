68f6dfb31f9d0f44b4167796b72867a2bde36e08
Aleksander <avolinec@gmail.com>
2019-10-15 11:24:24 +0300

    first commit

---
+++ resources/lang/en/multi.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'login' => 'Login',
+    'logout' => 'Logout',
+    'register' => 'Register',
+    'name' => 'Name',
+    'email' => 'E-Mail Address',
+    'pass' => 'Password',
+    'remember' => 'Remember Me',
+    'forgot' => 'Forgot Your Password?',
+    'send_link' => 'Send Password Reset Link',
+    'reset' => 'Reset Password',
+    'confirm' => 'Confirm Password',
+    'dash' => 'Dashboard',
+    'logged' => 'You are logged in!',
+];
---
+++ resources/lang/ru/multi.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'login' => 'Авторизоваться',
+    'logout' => 'Выйти',
+    'register' => 'Регистрироваться',
+    'name' => 'Имя',
+    'email' => 'Адрес эл. почты',
+    'pass' => 'Пароль',
+    'remember' => 'Запомнить меня',
+    'forgot' => 'Забыли пароль?',
+    'send_link' => 'Отправить ссылку для сброса пароля',
+    'reset' => 'Сброс пароля',
+    'confirm' => 'Подтвердите пароль',
+    'dash' => 'Кабинет',
+    'logged' => 'Вы вошли в систему!',
+];
